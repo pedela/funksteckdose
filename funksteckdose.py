48c8c56ef09e387233c624c35f25992904fc5238
PedeLa <lange@dbis.rwth-aachen.de>
2018-12-13 21:04:17 +0100

    added additional actor

--- funksteckdose.py
+++ funksteckdose.py
@@ -9,10 +9,10 @@ import time
 # GPIO init
 GPIO.setmode(GPIO.BOARD)
 
-names = ["Kühlschrank Klein Kalt","Kühlschrank Groß Kalt","Kühlschrank Groß Warm"]
-inputPins = [37,35,31]
-codes = ["11110 1","11110 2","11110 3"]
-states = [0,0,0]
+names = ["Kühltruhe kalt", "Kühlschrank kalt","Kühlschrank warm", "Kühltruhe warm"]
+inputPins = [37,35,31,40]
+codes = ["11110 1","11110 2","11110 3","11110 4"]
+states = [0,0,0,0]
 
 for i, inputPin in enumerate(inputPins):
   GPIO.setup(inputPin, GPIO.IN)
